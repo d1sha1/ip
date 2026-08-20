aa1d0d73c5c9959d7eb9d9ebac2513851e1bb2e6
disha <dishashivaraman@gmail.com>
2026-08-20 19:03:22 +0800

    Chatbot: Rename to Rocky, add greet and exit

---
+++ src/main/java/Rocky.java
@@ -0,0 +1,21 @@
+public class Rocky {
+    public static void main(String[] args) {
+        String banner =
+                " ____   ___   ____ _  ______   __\n"
+                        + "|  _ \\ / _ \\ / ___| |/ /\\ \\ / /\n"
+                        + "| |_) | | | | |   | ' /  \\ V / \n"
+                        + "|  _ <| |_| | |___| . \\   | |  \n"
+                        + "|_| \\_\\\\___/ \\____|_|\\_\\  |_|  \n";
+
+        String line = "________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Rocky.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
